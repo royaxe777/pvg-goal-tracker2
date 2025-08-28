1fcef8e4a02db6cd61d8b27451a90db444408ea6
royaxe777 <joomlastic@gmail.com>
2025-08-28 11:43:11 +0800

    Firebase COnfiguration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxl2bh7Qn5jqpWi87iakBKM0WgPC27uUU",
+  authDomain: "pvg-goal-tracker.firebaseapp.com",
+  projectId: "pvg-goal-tracker",
+  storageBucket: "pvg-goal-tracker.firebasestorage.app",
+  messagingSenderId: "478152124045",
+  appId: "1:478152124045:web:0c005204bd1c05a49224c4"
+};
+
+
+const app = initializeApp(firebaseConfig);
